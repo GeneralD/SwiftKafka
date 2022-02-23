4f634592a57610087cbafb9fc1ca457df3192432
GeneralD <yumejustice@gmail.com>
2022-02-23 14:33:08 +0900

    feature: get it compatible with M1 installed Apple

--- Sources/Crdkafka/shim.h
+++ Sources/Crdkafka/shim.h
@@ -1,7 +1,11 @@
 #ifndef rdkafka_shim_h
 #define rdkafka_shim_h
 #ifdef __APPLE__
+#ifdef __arm64__
+#include "/opt/homebrew/include/librdkafka/rdkafka.h"
+#else
 #include "/usr/local/include/librdkafka/rdkafka.h"
+#endif
 #else
 #include "/usr/include/librdkafka/rdkafka.h"
 #endif
